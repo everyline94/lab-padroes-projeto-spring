d6f86e2a7f7e771f186e37fc92e5ee499721e3d7
bruno larocca <brunolujanlarocca@gmail.com>
2023-10-19 18:37:09 -0300

    desafio entregue

---
+++ src/main/java/one/digitalinovation/labpadroesprojetospring/LabPadroesProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinovation.labpadroesprojetospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabPadroesProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPadroesProjetoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinovation/labpadroesprojetospring/LabPadroesProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinovation.labpadroesprojetospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabPadroesProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
